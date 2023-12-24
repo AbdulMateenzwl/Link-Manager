d5133f9f126aa34dd1037dbd557741027d9aecdb
AbdulMateenzwl <abdulmateenkhanzwl@gmail.com>
2023-12-23 20:32:20 -0800

    Add Apollo Client and GraphQL queries

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,30 +1,49 @@
 import Head from 'next/head';
 import { AwesomeLink } from '../components/AwesomeLink';
-import { links } from '../data/links';
+import { gql, useQuery } from '@apollo/client';
+
+const AllLinkQuery = gql`
+	query {
+		links {
+			id
+			title
+			url
+			description
+			category
+			imageUrl
+		}
+	}
+`;
 
 export default function Home() {
-  return (
-    <div>
-      <Head>
-        <title>Awesome Links</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+	const { data, error, loading } = useQuery(AllLinkQuery);
 
-      <div className="container mx-auto max-w-5xl my-20">
-        <ul className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
-          {links.map((link) => (
-            <AwesomeLink
-              key={link.id}
-              url={link.url}
-              id={link.id}
-              category={link.category}
-              title={link.title}
-              description={link.description}
-              imageUrl={link.imageUrl}
-            />
-          ))}
-        </ul>
-      </div>
-    </div>
-  );
+	if (loading) return <p>Loading ...</p>;
+
+	if (error) return <p>Oops! Something went wrong <br /> Error: {error.message}</p>;
+
+	return (
+		<div>
+			<Head>
+				<title>Awesome Links</title>
+				<link rel='icon' href='/favicon.ico' />
+			</Head>
+
+			<div className='container mx-auto max-w-5xl my-20'>
+				<ul className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5'>
+					{data?.links.map((link) => (
+						<AwesomeLink
+							key={link.id}
+							url={link.url}
+							id={link.id}
+							category={link.category}
+							title={link.title}
+							description={link.description}
+							imageUrl={link.imageUrl}
+						/>
+					))}
+				</ul>
+			</div>
+		</div>
+	);
 }
